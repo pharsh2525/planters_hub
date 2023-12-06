9c0c0704d55b34084938b53194b7a39cbd588059
Harsh Patel <p.harsh2525@gmail.com>
2023-12-06 13:20:51 -0600

    added admin user management

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,19 +1,61 @@
 ActiveAdmin.register User do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :first_name, :last_name, :is_admin, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:first_name, :last_name, :is_admin, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
   permit_params :email, :password, :password_confirmation, :first_name, :last_name, :is_admin
 
+  member_action :send_reset_password_instructions, method: :post do
+    user = User.find(params[:id])
+
+    # Trigger Devise reset password method
+    user.send_reset_password_instructions
+
+    redirect_to admin_user_path(user), notice: "Password reset instructions have been sent to #{user.email}"
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    column :is_admin
+    actions
+  end
+
+  filter :email
+  filter :first_name
+  filter :last_name
+  filter :is_admin
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :first_name
+      f.input :last_name
+      f.input :is_admin
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :first_name
+      row :last_name
+      row :is_admin
+      # Display other user attributes here
+      panel "Actions" do
+        span link_to "Send Reset Password Instructions", send_reset_password_instructions_admin_user_path(user), method: :post, data: { confirm: "Are you sure you want to send reset password instructions to this user?" }
+      end
+    end
+    active_admin_comments
+  end
+
+  controller do
+    def update
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete("password")
+        params[:user].delete("password_confirmation")
+      end
+      super
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :addresses
 
   validates :first_name, :last_name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    # If you have attributes that should not be searchable, exclude them here
+    super - ['encrypted_password', 'reset_password_token']
+  end
+
+  # Specify which associations should be searchable
+  def self.ransackable_associations(auth_object = nil)
+    # If there are associations like :passwords, exclude them here
+    super - ['some_sensitive_association']
+  end
 end
