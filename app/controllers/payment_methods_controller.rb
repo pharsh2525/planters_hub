9ea6b58c0f02e01522fcba04d9153f308b78e20f
Harsh Patel <p.harsh2525@gmail.com>
2023-12-07 20:25:55 -0600

    adding card to as payment methods

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -2,6 +2,16 @@
 class PaymentMethodsController < ApplicationController
   before_action :set_stripe_customer, only: [:new, :create]
 
+  def index
+    @payment_methods = Stripe::PaymentMethod.list(
+      customer: current_user.stripe_customer_id,
+      type: 'card'
+    )
+  rescue Stripe::StripeError => e
+    flash[:error] = e.message
+    redirect_to root_path
+  end
+
   def new
     # Display form for new payment method
   end
@@ -17,11 +27,23 @@ class PaymentMethodsController < ApplicationController
       @stripe_customer.invoice_settings.default_payment_method = payment_method.id
       @stripe_customer.save
 
-      redirect_to success_path, notice: 'Payment method added successfully.'
+      redirect_to payment_methods_path, notice: 'Payment method added successfully.'
     rescue Stripe::StripeError => e
-      redirect_to new_payment_method_path, alert: e.message
+      redirect_to new_payment_methods_path, alert: e.message
     end
   end
+
+  def destroy
+    payment_method = Stripe::PaymentMethod.retrieve(params[:id])
+    payment_method.detach
+
+    # Add any other logic you need here, such as removing references to the payment method in your database
+
+    redirect_to payment_methods_path, notice: 'Payment method removed successfully.'
+  rescue Stripe::StripeError => e
+    redirect_to payment_methods_path, alert: e.message
+  end
+
 
   private
 
