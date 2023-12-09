9ad04e6c9568a3c6b657eea885f722d944afce12
Harsh Patel <p.harsh2525@gmail.com>
2023-12-09 02:31:06 -0600

    added user side order view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,11 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   def index
-    @orders = current_user.orders
+    @orders = current_user.orders.order(created_at: :desc)
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order.order_items.includes(plant: { images_attachments: :blob })
   end
 
   # Add other CRUD actions as needed...
